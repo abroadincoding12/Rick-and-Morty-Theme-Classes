b3e983456367b13d2ec0cb555eac1c848bce365f
Iree McGirt <theradgamerivan@gmail.com>
2019-01-03 17:34:10 -0500

    Update SummerSmith.java

--- Inheritance Class (Rick and Morty Theme)/SummerSmith.java
+++ Inheritance Class (Rick and Morty Theme)/SummerSmith.java
@@ -1,5 +1,5 @@
 
-public class SummerSmith extends RickAndMortyShow{
+public class SummerSmith extends RickAndMortyShow{ //Summer is the sister of Morty and grandaughter of Rick
 	
 	public void characters() {
 		System.out.println("I'm the cool female from Rick and Morty");
