3db30b34abf7098cb1fd50523e0d308a9a7ae2b0
Iree McGirt <theradgamerivan@gmail.com>
2019-01-03 17:32:50 -0500

    Update MortySmith.java

--- Inheritance Class (Rick and Morty Theme)/MortySmith.java
+++ Inheritance Class (Rick and Morty Theme)/MortySmith.java
@@ -1,5 +1,5 @@
 
-public class MortySmith extends RickAndMortyShow{
+public class MortySmith extends RickAndMortyShow{ //Morty is the brother of Summer and grandson of Rick
 	
 	public void characters() {
 		System.out.println("I am a character from Rick and Morty");
